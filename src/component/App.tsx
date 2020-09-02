d918aa7786a58c516f5fe26c3231c72ecb45443c
Ulibka <gena68@inbox.ru>
2020-09-02 10:33:27 +0300

    first commit

---
+++ src/component/App.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import About from "./About";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  withRouter,
+} from "react-router-dom";
+
+// параметры определения пути
+// https://github.com/pillarjs/path-to-regexp#parameters
+
+class App extends React.Component<any, any> {
+  constructor(props?: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/forms">Формы</Link>
+            </li>
+          </ul>
+        </div>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/forms">
+            <div>
+              <h1>Урок9 формы</h1>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+// export default withRouter(App);
+export default App;
